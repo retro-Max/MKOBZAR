55c8ee8b7f4377472465b8516bd89ed98b15d4fc
Maxim Kobzar <mkobzar@mtrx-node06pd.dcm.senecacollege.ca>
2020-12-03 00:50:49 -0500

    added first draft of listSysUser() function

--- a2/A2S.py
+++ a2/A2S.py
@@ -54,6 +54,31 @@ def listSysUser():
     '''return a list of system (non-shell) user'''
     ...
 
+    # open file and save to list
+    f = open("/etc/passwd")
+    lines = f.readlines()
+    print(lines)
+
+    # test for sys user in list
+    sys_user = []
+    for line in lines:
+        if line.count("/sbin/nologin") != 0:
+            sys_user.append(line)
+    print(sys_user)
+    
+    # split user info by colon
+    parsed_user = []
+    for item in shell_user:
+        parsed_user.append(item.split(":"))
+    print(parsed_user)
+    
+    # create list of names of users
+    name_user = []
+    for user in parsed_user:
+        name_user.append(user[0])
+    print(name_user)
+
+
 
 def findUser(name):
     '''find user with a given user name'''
